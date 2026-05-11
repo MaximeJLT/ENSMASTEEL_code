f08e452060998c10e0c939940e5934e95a503354
Onyx7502025 <81749637+Onyx75020@users.noreply.github.com>
2026-05-12 00:29:22 +0200

    Update code_lidar.ino

--- motherboard/code_lidar.ino
+++ motherboard/code_lidar.ino
@@ -1,42 +1,44 @@
 
+#include <Arduino.h>
 void setup() {
-    Serial.begin(115200);         // Debug USB
-    Serial1.begin(115200, SERIAL_8N1, 0, 1);   // UART1 = pins 0 (RX) et 1 (TX)
+    Serial.begin(115200); // Debug USB
+    Serial1.begin(230400, SERIAL_8N1, 0, 1);  // UART1 : RX=0  TX=1
 }
 
 
-
-int lireDistanceDTOF() {
-    if (Serial1.available() < 9) return -1;
-    // Chercher l’en-tête 0x59 0x59
-    if (Serial1.read() != 0x59) return -1;
-    if (Serial1.read() != 0x59) return -1;
-    uint8_t distL = Serial1.read();
-    uint8_t distH = Serial1.read();
-    uint8_t strengthL = Serial1.read();
-    uint8_t strengthH = Serial1.read();
-    uint8_t tempL = Serial1.read();
-    uint8_t tempH = Serial1.read();
-    uint8_t checksum = Serial1.read();
-    int distance = (distH << 8) | distL;
-    return distance;
+bool lireMesure(int &angleDeg, int &distanceMm) {
+    if (!Serial1.available()) return false;
+    if (Serial1.read() != 0x54) return false;
+    while (Serial1.available() < 11);
+    uint8_t frame[11];
+    for (int i = 0; i < 11; i++)
+        frame[i] = Serial1.read();
+    int angleRaw = (frame[4] << 8 | frame[3]);
+    angleDeg = angleRaw / 100;  // -> 0–359°
+    distanceMm = (frame[6] << 8 | frame[5]);
+    return true;
 }
 
 
-
-int distanceFiltree(int minDist) {
-    int dist = lireDistanceDTOF();
-    if (dist < 0) return -1;         
-    if (dist < minDist) return -1;    
-    return dist;
+void detectionObjet(int seuil_mm) {
+    int angleDeg, distMm;
+    if (lireMesure(angleDeg, distMm)) {
+        // Conversion degrés → radians
+        float angleRad = angleDeg * 3.141592653589793f / 180.0f;
+        if (distMm > 0 && distMm < seuil_mm) {
+            Serial.print("OBST ");
+            Serial.print(distMm);
+            Serial.print(" ");
+            Serial.print(angleRad, 4);
+            Serial.println("");
+        }
+        else {
+            Serial.println("CLEAR");
+        }
+    }
 }
-
 
 
 void loop() {
-    int dist = distanceFiltree(100);   // veut un objet à minimum 100 mm
-    if (dist = -1 ) {
-        printf("STOP"); //cette ligne sert à gérer les arrets à remplacer par une alerte
-    }
-    delay(50); // cette ligne est là pour assurer le bon "echantilloanage du lidar"
+    detectionObjet(300);   // seuil = 300 mm
 }
